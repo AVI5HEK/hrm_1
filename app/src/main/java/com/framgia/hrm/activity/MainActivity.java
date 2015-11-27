33cc46e2f56b32dee342034868b1514e89b1e934
AVI5HEK <avishek.khan.official@gmail.com>
2015-11-27 15:15:37 +0600

    Add_Staff

--- app/src/main/java/com/framgia/hrm/activity/MainActivity.java
+++ app/src/main/java/com/framgia/hrm/activity/MainActivity.java
@@ -1,10 +1,9 @@
 package com.framgia.hrm.activity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
@@ -12,6 +11,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.framgia.hrm.R;
@@ -35,10 +35,10 @@ public class MainActivity extends AppCompatActivity implements FragmentDrawer.Fr
     public static SharedPreferences PREF_STATE=null;
     public static SharedPreferences.Editor editor=null;
     int state=0;
-    DatabaseHelper mdDatabaseHelper;
+    private static final String BUNDLE_ID = "id";
+    private DatabaseHelper mDatabaseHelper;
     MyAdapter adapter;
     ListView list_department;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,51 +46,60 @@ public class MainActivity extends AppCompatActivity implements FragmentDrawer.Fr
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        mdDatabaseHelper=new DatabaseHelper(this);
+        mDatabaseHelper =new DatabaseHelper(this);
         list_department=(ListView)findViewById(R.id.list_department);
-
-        PREF_STATE = getApplicationContext().getSharedPreferences("MyPref",
+        /*PREF_STATE = getApplicationContext().getSharedPreferences("MyPref",
                 MODE_PRIVATE);
-        int pref_State = PREF_STATE.getInt("state",0);
+        int pref_State = PREF_STATE.getInt("state",0);*/
         drawerFragment = (FragmentDrawer) getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
         drawerFragment.setDrawerListener(this);
         displayView(0);
-
-        if(pref_State!=3) {
-            mdDatabaseHelper.createDepartment(new Department("HRM & ADMIN"));
-            mdDatabaseHelper.createDepartment(new Department("FINANCE & ACCOUNTS"));
-            mdDatabaseHelper.createDepartment(new Department("SELLS"));
-            mdDatabaseHelper.createDepartment(new Department("MARKETING"));
-            mdDatabaseHelper.createDepartment(new Department("OPERATIONS"));
-            mdDatabaseHelper.createStatus(new Status("TRAINEE"));
-            mdDatabaseHelper.createStatus(new Status("INTERNSHIP"));
-            mdDatabaseHelper.createStatus(new Status("OFFICIAL STAFF"));
-            mdDatabaseHelper.createPosition(new Position("CEO"));
-            mdDatabaseHelper.createPosition(new Position("MANAGER"));
-            mdDatabaseHelper.createPosition(new Position("DEPUTY MANAGER"));
-            mdDatabaseHelper.createPosition(new Position("SR. EXECUTIVE"));
-            mdDatabaseHelper.createPosition(new Position("EXECUTIVE"));
-            mdDatabaseHelper.createActivity(new Activity("ACTIVE"));
-            mdDatabaseHelper.createActivity(new Activity("INACTIVE"));
-
+        //if(pref_State!=3)
+        if (!mDatabaseHelper.departmentTableExists() && !mDatabaseHelper.statusTableExists() &&
+                !mDatabaseHelper.positionTableExists() && !mDatabaseHelper.activityTableExists()) {
+            mDatabaseHelper.createDepartment(new Department("HRM & ADMIN"));
+            mDatabaseHelper.createDepartment(new Department("FINANCE & ACCOUNTS"));
+            mDatabaseHelper.createDepartment(new Department("SELLS"));
+            mDatabaseHelper.createDepartment(new Department("MARKETING"));
+            mDatabaseHelper.createDepartment(new Department("OPERATIONS"));
+            mDatabaseHelper.createStatus(new Status("TRAINEE"));
+            mDatabaseHelper.createStatus(new Status("INTERNSHIP"));
+            mDatabaseHelper.createStatus(new Status("OFFICIAL STAFF"));
+            mDatabaseHelper.createPosition(new Position("CEO"));
+            mDatabaseHelper.createPosition(new Position("MANAGER"));
+            mDatabaseHelper.createPosition(new Position("DEPUTY MANAGER"));
+            mDatabaseHelper.createPosition(new Position("SR. EXECUTIVE"));
+            mDatabaseHelper.createPosition(new Position("EXECUTIVE"));
+            mDatabaseHelper.createActivity(new Activity("ACTIVE"));
+            mDatabaseHelper.createActivity(new Activity("INACTIVE"));
         }
-        editor = PREF_STATE.edit();
+        /*editor = PREF_STATE.edit();
         editor.putInt("state", 3);
-        editor.commit();
-        ArrayList<Department> arrayList=mdDatabaseHelper.getAllDepartments();
+        editor.commit();*/
+        ArrayList<Department> arrayList= mDatabaseHelper.getAllDepartments();
         adapter=new MyAdapter(this,arrayList);
         list_department.setAdapter(adapter);
-
+        list_department.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int idToSearch = position + 1;
+                Bundle dataBundle = new Bundle();
+                dataBundle.putLong(BUNDLE_ID, idToSearch);
+                Intent intent = new Intent(getApplicationContext(), com.framgia.hrm.activity
+                        .StaffListActivity.class);
+                intent.putExtras(dataBundle);
+                startActivity(intent);
+            }
+        });
+        mDatabaseHelper.closeDB();
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -102,12 +111,10 @@ public class MainActivity extends AppCompatActivity implements FragmentDrawer.Fr
         }
         return super.onOptionsItemSelected(item);
     }
-
     @Override
     public void onDrawerItemSelected(View view, int position) {
         displayView(position);
     }
-
     private void displayView(int position) {
         Fragment fragment = null;
         String title = getString(R.string.app_name);
@@ -117,7 +124,10 @@ public class MainActivity extends AppCompatActivity implements FragmentDrawer.Fr
                 title = getString(R.string.title_home);
                 break;
             case REGISTRATION_FRAGMENT:
-                fragment = new RegistrationFragment();
+                /*fragment = new RegistrationFragment();
+                title = getString(R.string.title_registration);*/
+                Intent intent = new Intent(getApplicationContext(), Addstaff.class);
+                startActivity(intent);
                 title = getString(R.string.title_registration);
                 break;
             case VIEW_FRAGMENT:
@@ -139,12 +149,10 @@ public class MainActivity extends AppCompatActivity implements FragmentDrawer.Fr
             getSupportActionBar().setTitle(title);
         }*/
     }
-
     @Override
     public boolean onQueryTextSubmit(String query) {
         return false;
     }
-
     @Override
     public boolean onQueryTextChange(String newText) {
         return false;
--- app/src/main/java/com/framgia/hrm/database/DatabaseHelper.java
+++ app/src/main/java/com/framgia/hrm/database/DatabaseHelper.java
@@ -6,6 +6,8 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+
+import com.framgia.hrm.model.Activity;
 import com.framgia.hrm.model.Department;
 import com.framgia.hrm.model.Position;
 import com.framgia.hrm.model.SearchStaff;
@@ -226,13 +228,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     /**
      * Creating activity
      */
-    public long createActivity(com.framgia.hrm.model.Activity act) {
+    public long createActivity(Activity act) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put(ACTIVITY_COLUMN_NAME, act.getActivity_name());
         long act_id = db.insert(TABLE_ACTIVITY, null, values);
         return act_id;
     }
+
     /**
      * Creating staff
      */
@@ -250,10 +253,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         long staff_id = db.insert(TABLE_STAFF, null, values);
         return staff_id;
     }
+
+    /**Reader methods*/
+
     /**
      * Getting a single Department
      */
-    public Department getDepartment(long dept_id) {
+    /*public Department getDepartment(long dept_id) {
         SQLiteDatabase db = this.getReadableDatabase();
         String selectQuery = "SELECT * FROM " + TABLE_DEPARTMENT + " WHERE " + DEPARTMENT_COLUMN_ID + " = " +
                 dept_id;
@@ -263,104 +269,100 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         dept.setDept_id(c.getInt(c.getColumnIndex(DEPARTMENT_COLUMN_ID)));
         dept.setDept_name(c.getString(c.getColumnIndex(DEPARTMENT_COLUMN_NAME)));
         return dept;
-    }
-    /**
-     * get department id
-     */
-    public int getDepartmentid(String department) {
-        SQLiteDatabase db = this.getReadableDatabase();
-        int id = 0;
-        String query = "SELECT " + DEPARTMENT_COLUMN_ID + " FROM " + TABLE_DEPARTMENT + " where "
-                + DEPARTMENT_COLUMN_NAME + " = ?";
-        Cursor cursor = db.rawQuery(query, new String[]{department});
-        if (cursor != null && cursor.getCount() > 0) {
-            cursor.moveToFirst();
-            id = cursor.getInt(cursor.getColumnIndex(DEPARTMENT_COLUMN_ID));
-        } else
-            Log.e("problem", "department id  is not inculded in department table");
-        cursor.close();
-        db.close();
-        return id;
-    }
-    /**
-     * get last staff id
-     */
-    public int laststaffid(){
+    }*/
 
-        SQLiteDatabase db = this.getReadableDatabase();
-        int id=0;
-        String query = "SELECT " + STAFF_COLUMN_ID + " FROM " + TABLE_STAFF ;
-        Cursor cursor = db.rawQuery(query,null);
-        if (cursor != null && cursor.getCount() > 0) {
-            cursor.moveToLast();
-            id = cursor.getInt(cursor.getColumnIndex(STAFF_COLUMN_ID));
-        } else
-            Log.e("problem", "status id  is not inculded in status table");
-        cursor.close();
-        db.close();
-        return id;
-    }
     /**
-     * get status id
+     * Getting all the Departments
+     * this needs to changed. ArrayList >> List
      */
-    public int getstatusid(String status) {
+    public ArrayList<Department> getAllDepartments() {
+        ArrayList<Department> departments = new ArrayList<Department>();
+        String selectQuery = "SELECT * FROM " + TABLE_DEPARTMENT;
         SQLiteDatabase db = this.getReadableDatabase();
-        int id = 0;
-        String query = "SELECT " + STATUS_COLUMN_ID + " FROM " + TABLE_STATUS + " where "
-                + STATUS_COLUMN_NAME + " = ?";
-        Cursor cursor = db.rawQuery(query, new String[]{status});
-        if (cursor != null && cursor.getCount() > 0) {
-            cursor.moveToFirst();
-            id = cursor.getInt(cursor.getColumnIndex(STATUS_COLUMN_ID));
-        } else
-            Log.e("problem", "status id  is not inculded in status table");
+        Cursor c = db.rawQuery(selectQuery, null);
+        /**looping through all rows and adding to list*/
+        if (c.moveToFirst()) {
+            do {
+                Department dept = new Department();
+                dept.setDept_id(c.getInt(c.getColumnIndex(DEPARTMENT_COLUMN_ID)));
+                dept.setDept_name(c.getString(c.getColumnIndex(DEPARTMENT_COLUMN_NAME)));
 
-        cursor.close();
-        db.close();
-        return id;
+                departments.add(dept);
+            } while (c.moveToNext());
+        }
+        return departments;
     }
-    /**
-     * get position id
-     */
-    public int getpostionid(String position) {
-        SQLiteDatabase db = this.getReadableDatabase();
-        int id = 0;
-        String query = "SELECT " + POSITION_COLUMN_ID + " FROM " + TABLE_POSITION + " where "
-                + POSITION_COLUMN_NAME + " = ?";
-        Cursor cursor = db.rawQuery(query, new String[]{position});
-        if (cursor != null && cursor.getCount() > 0) {
-            cursor.moveToFirst();
-            id = cursor.getInt(cursor.getColumnIndex(POSITION_COLUMN_ID));
-        } else
-            Log.e("problem", "position id  is not inculded in position table");
-        cursor.close();
-        db.close();
-        return id;
-    }
-    /**
-     * get Activity id
-     */
-    public int getActivityid(String activity) {
-        SQLiteDatabase db = this.getReadableDatabase();
-        int id = 0;
-        String query = "SELECT " + ACTIVITY_COLUMN_ID + " FROM " + TABLE_ACTIVITY + " where "
-                + ACTIVITY_COLUMN_NAME + " = ?";
-        Cursor cursor = db.rawQuery(query, new String[]{activity});
-        if (cursor != null && cursor.getCount() > 0) {
-            cursor.moveToFirst();
-            id = cursor.getInt(cursor.getColumnIndex(ACTIVITY_COLUMN_ID));
-        } else
-            Log.e("problem", "activity id  is not inculded in activity table");
-        cursor.close();
-        db.close();
-        return id;
 
+    /**
+     * Getting staffs by department
+     */
+    public List<Staff> getStaffByDepartment(long dept_id) {
+        ArrayList<Staff> staffs = new ArrayList<Staff>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        String selectQuery = "SELECT * FROM " + TABLE_STAFF + " WHERE " + STAFF_COLUMN_DEPT + " =" +
+                " " + dept_id;
+        Cursor c = db.rawQuery(selectQuery, null);
+        if (c != null) c.moveToFirst();
+        if (c.moveToFirst()) {
+            do {
+                Staff staff = new Staff();
+                staff.setStaff_id(c.getInt(c.getColumnIndex(STAFF_COLUMN_ID)));
+                staff.setName(c.getString(c.getColumnIndex(STAFF_COLUMN_NAME)));
+                staff.setDate_of_birth(c.getString(c.getColumnIndex(STAFF_COLUMN_DATE_OF_BIRTH)));
+                staff.setBirth_place(c.getString(c.getColumnIndex(STAFF_COLUMN_BIRTH_PLACE)));
+                staff.setPhone_number(c.getString(c.getColumnIndex(STAFF_COLUMN_PHONE_NUMBER)));
+                staff.setDept_id(c.getInt(c.getColumnIndex(STAFF_COLUMN_DEPT)));
+                staff.setStatus_id(c.getInt(c.getColumnIndex(STAFF_COLUMN_STATUS)));
+                staff.setActivity_id(c.getInt(c.getColumnIndex(STAFF_COLUMN_ACTIVITY)));
+                staff.setPosition_id(c.getInt(c.getColumnIndex(STAFF_COLUMN_POSITION)));
+                staffs.add(staff);
+            } while (c.moveToNext());
+        }
+        return staffs;
     }
+    /*public ArrayList getStaffByDepartment(long dept_id) {
+        ArrayList<Staff> staffs = new ArrayList<Staff>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        String selectQuery = "SELECT * FROM " + TABLE_STAFF + " INNER JOIN " + TABLE_DEPARTMENT + " " +
+                "ON " + TABLE_STAFF + "." + STAFF_COLUMN_DEPT + " = " + TABLE_DEPARTMENT + "" +
+                "." + DEPARTMENT_COLUMN_ID + "WHERE " + TABLE_STAFF + "." + STAFF_COLUMN_DEPT + " = " +
+                dept_id;
+
+        Cursor c = db.rawQuery(selectQuery, null);
+        ArrayList arrayListName = new ArrayList();
+        c.moveToFirst();
+        while(c.isAfterLast() == false){
+            arrayListName.add(c.getString(c.getColumnIndex("staff_id")));
+            arrayListName.add(c.getString(c.getColumnIndex("name")));
+        }
+
+        *//*if (c != null) c.moveToFirst();
+
+        if (c.moveToFirst()) {
+            do {
+                Staff staff = new Staff();
+                staff.setStaff_id(c.getInt(c.getColumnIndex(STAFF_COLUMN_ID)));
+                staff.setName(c.getString(c.getColumnIndex(STAFF_COLUMN_NAME)));
+                staff.setDate_of_birth(c.getString(c.getColumnIndex(STAFF_COLUMN_DATE_OF_BIRTH)));
+                staff.setBirth_place(c.getString(c.getColumnIndex(STAFF_COLUMN_BIRTH_PLACE)));
+                staff.setPhone_number(c.getString(c.getColumnIndex(STAFF_COLUMN_PHONE_NUMBER)));
+                staff.setDept_id(c.getInt(c.getColumnIndex(STAFF_COLUMN_DEPT)));
+                staff.setStatus_id(c.getInt(c.getColumnIndex(STAFF_COLUMN_STATUS)));
+                staff.setActivity_id(c.getInt(c.getColumnIndex(STAFF_COLUMN_ACTIVITY)));
+                staff.setPosition_id(c.getInt(c.getColumnIndex(STAFF_COLUMN_POSITION)));
+
+                staffs.add(staff);
+            } while (c.moveToNext());
+        }
+
+        return staffs;*//*
+        return arrayListName;
+    }*/
+
+    /**the tables below will be used later*/
     /**
      * Getting staff by id
      */
-
-
     public Staff getStaffById(long staff_id) {
         SQLiteDatabase db = this.getReadableDatabase();
         String selectQuery = "SELECT * FROM " + TABLE_STAFF + " WHERE " + STAFF_COLUMN_ID + " =" +
@@ -379,56 +381,100 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         staff.setPosition_id(c.getInt(c.getColumnIndex(STAFF_COLUMN_POSITION)));
         return staff;
     }
-    public ArrayList<Department> getAllDepartments() {
-        ArrayList departments = new ArrayList();
-        String selectQuery = "SELECT * FROM " + TABLE_DEPARTMENT;
+
+    /**
+     * Getting department by id
+     */
+    public Department getDepartmentById(long dept_id) {
         SQLiteDatabase db = this.getReadableDatabase();
+        String selectQuery = "SELECT * FROM " + TABLE_DEPARTMENT + " WHERE " +
+                DEPARTMENT_COLUMN_ID + " = " + dept_id;
         Cursor c = db.rawQuery(selectQuery, null);
-        /**looping through all rows and adding to list*/
-        if (c.moveToFirst()) {
-            do {
-                //Department dept = new Department();
-                //dept.setDept_id(c.getInt(c.getColumnIndex(DEPARTMENT_COLUMN_ID)));
-                //dept.setDept_name(c.getString(c.getColumnIndex(DEPARTMENT_COLUMN_NAME)));
-                String name = c.getString(c.getColumnIndex(DEPARTMENT_COLUMN_NAME));
-                int id = c.getInt(c.getColumnIndex(DEPARTMENT_COLUMN_ID));
-                Department dept = new Department(id, name);
-                //  departments.add(c.getString(c.getColumnIndex(DEPARTMENT_COLUMN_NAME)));
-                // departments.add(c.getString(c.getColumnIndex(DEPARTMENT_COLUMN_NAME)));
-                departments.add(dept);
-            } while (c.moveToNext());
-        }
-        return departments;
+        Department dept = new Department();
+        if (c != null) c.moveToFirst();
+        dept.setDept_id(c.getInt(c.getColumnIndex(DEPARTMENT_COLUMN_ID)));
+        dept.setDept_name(c.getString(c.getColumnIndex(DEPARTMENT_COLUMN_NAME)));
+        return dept;
     }
 
     /**
-     * Getting staffs by department
+     * Getting status by id
      */
-    public List<Staff> getStaffByDepartment(long dept_id) {
-        ArrayList<Staff> staffs = new ArrayList<Staff>();
+    public Status getStatusById(long status_id) {
         SQLiteDatabase db = this.getReadableDatabase();
-        String selectQuery = "SELECT * FROM " + TABLE_STAFF + " WHERE " + STAFF_COLUMN_DEPT + " =" +
-                " " + dept_id;
+        String selectQuery = "SELECT * FROM " + TABLE_STATUS + " WHERE " + STATUS_COLUMN_ID +
+                " =" +
+                " " + status_id;
         Cursor c = db.rawQuery(selectQuery, null);
-        if (c != null && c.getCount()>0) c.moveToFirst();
-        if (c.moveToFirst()) {
-            do {
-                Staff staff = new Staff();
-                staff.setStaff_id(c.getInt(c.getColumnIndex(STAFF_COLUMN_ID)));
-                staff.setName(c.getString(c.getColumnIndex(STAFF_COLUMN_NAME)));
-                staff.setDate_of_birth(c.getString(c.getColumnIndex(STAFF_COLUMN_DATE_OF_BIRTH)));
-                staff.setBirth_place(c.getString(c.getColumnIndex(STAFF_COLUMN_BIRTH_PLACE)));
-                staff.setPhone_number(c.getString(c.getColumnIndex(STAFF_COLUMN_PHONE_NUMBER)));
-                staff.setDept_id(c.getInt(c.getColumnIndex(STAFF_COLUMN_DEPT)));
-                staff.setStatus_id(c.getInt(c.getColumnIndex(STAFF_COLUMN_STATUS)));
-                staff.setActivity_id(c.getInt(c.getColumnIndex(STAFF_COLUMN_ACTIVITY)));
-                staff.setPosition_id(c.getInt(c.getColumnIndex(STAFF_COLUMN_POSITION)));
-                staffs.add(staff);
-            } while (c.moveToNext());
-        }
-
-        return staffs;
+        Status status = new Status();
+        if (c != null) c.moveToFirst();
+        status.setStatus_id(c.getInt(c.getColumnIndex(STATUS_COLUMN_ID)));
+        status.setStatus_name(c.getString(c.getColumnIndex(STATUS_COLUMN_NAME)));
+        return status;
     }
+
+    /**
+     * Getting position by id
+     */
+    public Position getPositionById(long pos_id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String selectQuery = "SELECT * FROM " + TABLE_POSITION + " WHERE " + POSITION_COLUMN_ID +
+                " = " + pos_id;
+        Cursor c = db.rawQuery(selectQuery, null);
+        Position pos = new Position();
+        if (c != null) c.moveToFirst();
+        pos.setPosition_id(c.getInt(c.getColumnIndex(POSITION_COLUMN_ID)));
+        pos.setPosition_name(c.getString(c.getColumnIndex(POSITION_COLUMN_NAME)));
+        return pos;
+    }
+
+    /**
+     * Getting activity by id
+     */
+    public Activity getActivityById(long act_id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String selectQuery = "SELECT * FROM " + TABLE_ACTIVITY + " WHERE " + ACTIVITY_COLUMN_ID +
+                " = " + act_id;
+        Cursor c = db.rawQuery(selectQuery, null);
+        Activity act = new Activity();
+        if (c != null) c.moveToFirst();
+        act.setActivity_id(c.getInt(c.getColumnIndex(ACTIVITY_COLUMN_ID)));
+        act.setActivity_name(c.getString(c.getColumnIndex(ACTIVITY_COLUMN_NAME)));
+        return act;
+    }
+
+    /** Update methods */
+
+    /**
+     * Update staff by id
+     */
+    public int updateStaff(Staff staff) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(STAFF_COLUMN_NAME, staff.getName());
+        values.put(STAFF_COLUMN_DATE_OF_BIRTH, staff.getDate_of_birth());
+        values.put(STAFF_COLUMN_BIRTH_PLACE, staff.getBirth_place());
+        values.put(STAFF_COLUMN_PHONE_NUMBER, staff.getPhone_number());
+        values.put(STAFF_COLUMN_DEPT, staff.getDept_id());
+        values.put(STAFF_COLUMN_STATUS, staff.getStatus_id());
+        values.put(STAFF_COLUMN_ACTIVITY, staff.getActivity_id());
+        values.put(STAFF_COLUMN_POSITION, staff.getPosition_id());
+
+        // updating row
+        return db.update(TABLE_STAFF, values, STAFF_COLUMN_ID + " = ?",
+                new String[]{String.valueOf(staff.getStaff_id())});
+    }
+    /** Delete methods */
+
+    /**
+     * Delete staff by id
+     */
+    public void deleteStaff(long staff_id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_STAFF, STAFF_COLUMN_ID + " = ?",
+                new String[]{String.valueOf(staff_id)});
+    }
+
     /**
      * Checks if department table exists
      */
@@ -448,6 +494,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             return false;
         }
     }
+
     /**
      * Checks if status table exists
      */
@@ -467,6 +514,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             return false;
         }
     }
+
     /**
      * Checks if position table exists
      */
@@ -486,6 +534,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             return false;
         }
     }
+
     /**
      * Checks if activity table exists
      */
@@ -525,6 +574,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             return false;
         }
     }
+
     /**
      * closing database
      */
